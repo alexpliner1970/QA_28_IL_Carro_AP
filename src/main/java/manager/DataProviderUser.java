93955d90b45f3261eeaf291a3c4125121d52e79f
alexpliner1970 <alexpliner3005@gmail.com>
2025-03-11 19:24:31 +0200

    file_run

--- src/main/java/manager/DataProviderUser.java
+++ src/main/java/manager/DataProviderUser.java
@@ -10,16 +10,19 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 public class DataProviderUser {
     @DataProvider
     public Iterator<Object[]> registrationFile() throws IOException {
+
+        int i = new Random().nextInt(1000) + 1000;
         List<Object[]> list=new ArrayList<>();
         BufferedReader reader=new BufferedReader(new FileReader(new File("src/test/resources/reg_date.csv")));
         String line=reader.readLine();
         while (line!=null){
             String[]all=line.split(",");
-            list.add(new Object[]{new User().setFirsName(all[0]).setLastName(all[1]).setEmail(all[2]).setPassword(all[3])});
+            list.add(new Object[]{new User().setFirsName(all[0]).setLastName(all[1]).setEmail(i+all[2]).setPassword(all[3])});
             line=reader.readLine();
         }
 
